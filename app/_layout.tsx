d9ec8fdfdd5949e6d47f047a0e1cf2a95f0eacb0
David Tepan <jtepan10@gmail.com>
2025-11-30 22:58:33 -0500

    refactor: Revert splash screen changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,21 +4,27 @@
 import { useFonts, SpaceMono_400Regular } from '@expo-google-fonts/space-mono';
 import { Stack } from 'expo-router';
 import { AuthProvider } from '../contexts/AuthContext';
-import CustomSplashScreen from './splash';
+import { useEffect } from 'react';
+import { SplashScreen } from 'expo-router';
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
     SpaceMono: SpaceMono_400Regular,
   });
 
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
-    return <CustomSplashScreen />;
+    return <SplashScreen />;
   }
 
   return (
     <AuthProvider>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="splash" />
         {/* auth screens */}
         <Stack.Screen name="login" />
         <Stack.Screen name="signup" />
